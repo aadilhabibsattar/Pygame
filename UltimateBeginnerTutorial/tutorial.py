7989982f1dc08e63ae5f386d29602e212fdfda2d
ArloTheOriginal <100754261+ArloTheOriginal@users.noreply.github.com>
2022-03-06 21:36:56 +0600

    Update tutorial.py

--- UltimateBeginnerTutorial/tutorial.py
+++ UltimateBeginnerTutorial/tutorial.py
@@ -5,17 +5,17 @@ from random import randint, choice
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        player_walk_1 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/player_walk_1.png").convert_alpha()
-        player_walk_2 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/player_walk_2.png").convert_alpha()
+        player_walk_1 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/player_walk_1.png").convert_alpha()
+        player_walk_2 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/player_walk_2.png").convert_alpha()
         self.player_walk = [player_walk_1,player_walk_2]
         self.player_index = 0
-        self.player_jump = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/jump.png").convert_alpha()
+        self.player_jump = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/jump.png").convert_alpha()
         
         self.image = self.player_walk[self.player_index]
         self.rect = self.image.get_rect(midbottom = (80, 300))
         self.gravity = 0
         
-        self.jump_sound = pygame.mixer.Sound("P:/Python/Games/UltimateBeginnerTutorial/Audio/jump.mp3")
+        self.jump_sound = pygame.mixer.Sound("P:/Python/Pygame/UltimateBeginnerTutorial/Audio/jump.mp3")
         self.jump_sound.set_volume(0.01)
         
     def player_input(self):
@@ -48,13 +48,13 @@ class Obstacle(pygame.sprite.Sprite):
         super().__init__()
         
         if type == 'fly':
-            fly_1 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/fly/fly1.png").convert_alpha()
+            fly_1 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/fly/fly1.png").convert_alpha()
             fly_2 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/fly/fly2.png").convert_alpha()
             self.frames = [fly_1,fly_2]
             y_pos = 210
         else:
-            snail_1 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/snail/snail1.png").convert_alpha()
-            snail_2 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/snail/snail2.png").convert_alpha()
+            snail_1 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/snail/snail1.png").convert_alpha()
+            snail_2 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/snail/snail2.png").convert_alpha()
             self.frames = [snail_1,snail_2]
             y_pos = 300
 
@@ -123,11 +123,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Pixel Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('P:/Python/Games/UltimateBeginnerTutorial/font/Pixeltype.ttf', 50)
+test_font = pygame.font.Font('P:/Python/Pygame/UltimateBeginnerTutorial/font/Pixeltype.ttf', 50)
 game_active = False
 start_time = 0
 score = 0
-bg_music = pygame.mixer.Sound("P:/Python/Games/UltimateBeginnerTutorial/Audio/music.wav")
+bg_music = pygame.mixer.Sound("P:/Python/Pygame/UltimateBeginnerTutorial/Audio/music.wav")
 bg_music.set_volume(0.01)
 bg_music.play(loops= -1)
 
@@ -138,19 +138,19 @@ player.add(Player())
 obstacle_group = pygame.sprite.Group()
 
 # Assets
-sky_surface = pygame.image.load('P:/Python/Games/UltimateBeginnerTutorial/graphics/Sky.png').convert()
-ground_surface = pygame.image.load('P:/Python/Games/UltimateBeginnerTutorial/graphics/ground.png').convert()
+sky_surface = pygame.image.load('P:/Python/Pygame/UltimateBeginnerTutorial/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('P:/Python/Pygame/UltimateBeginnerTutorial/graphics/ground.png').convert()
 
 # Snail
-snail_frame_1 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/snail/snail1.png").convert_alpha()
-snail_frame_2 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/snail/snail2.png").convert_alpha()
+snail_frame_1 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/snail/snail1.png").convert_alpha()
+snail_frame_2 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/snail/snail2.png").convert_alpha()
 snail_frames = [snail_frame_1,snail_frame_2]
 snail_frame_index = 0
 snail_surf = snail_frames[snail_frame_index]
 
 # Fly
-fly_frame_1 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/fly/fly1.png").convert_alpha()
-fly_frame_2 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/fly/fly2.png").convert_alpha()
+fly_frame_1 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/fly/fly1.png").convert_alpha()
+fly_frame_2 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/fly/fly2.png").convert_alpha()
 fly_frames = [fly_frame_1,fly_frame_2]
 fly_frame_index = 0
 fly_surf = fly_frames[fly_frame_index]
@@ -158,18 +158,18 @@ fly_surf = fly_frames[fly_frame_index]
 obstacle_rect_list = []
 
 # Player
-player_walk_1 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/player_walk_1.png").convert_alpha()
-player_walk_2 = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/player_walk_2.png").convert_alpha()
+player_walk_1 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/player_walk_1.png").convert_alpha()
+player_walk_2 = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/player_walk_2.png").convert_alpha()
 player_walk = [player_walk_1,player_walk_2]
 player_index = 0
-player_jump = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/jump.png").convert_alpha()
+player_jump = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/jump.png").convert_alpha()
 
 player_surf = player_walk[player_index]
 player_rect = player_surf.get_rect(midbottom = (80, 300))
 player_gravity = 0
 
 # Intro screen
-player_stand = pygame.image.load("P:/Python/Games/UltimateBeginnerTutorial/graphics/player/player_stand.png").convert_alpha()
+player_stand = pygame.image.load("P:/Python/Pygame/UltimateBeginnerTutorial/graphics/player/player_stand.png").convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center = (400, 200))
 
